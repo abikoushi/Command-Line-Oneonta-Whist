12f3b15b9e6f75b18f881da8e24bae308c9be1ff
abikoushi <a.b.e.k.o.69@gmail.com>
2017-08-17 00:30:48 +0900

    Initial Commit

---
+++ Command-Line-Oneonta-Whist/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Command-Line-Oneonta-Whist
+//
+//  Created by ko abe on 2017/08/17.
+//  Copyright © 2017年 abiko ushi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
